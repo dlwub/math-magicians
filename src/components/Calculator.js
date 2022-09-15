af537536e4d349ab285855a976e01b08522f6b26
dlwub <dlwubi@gmail.com>
2022-09-16 00:10:03 +0300

    Add logic to calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,37 +1,46 @@
 import React from 'react';
 import Button from './Button';
+import calculate from '../logic/calculate';
 
 export default class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: 0,
+      next: null,
+      operation: null,
+    };
   }
 
-  render() {
-    return (
-      <div className="button-container">
-        <Button value={0} className="btn row1" />
-        <Button value="A/C" className="btn" />
-        <Button value="+/-" className="btn" />
-        <Button value="%" className="btn" />
-        <Button value="÷" className="btn orange" />
-        <Button value={7} className="btn" />
-        <Button value={8} className="btn" />
-        <Button value={9} className="btn" />
-        <Button value="×" className="btn orange" />
-        <Button value={4} className="btn" />
-        <Button value={5} className="btn" />
-        <Button value={6} className="btn" />
-        <Button value="-" className="btn orange" />
-        <Button value={1} className="btn" />
-        <Button value={2} className="btn" />
-        <Button value={3} className="btn" />
-        <Button value="+" className="btn orange" />
-        <Button value={0} className="btn col2" />
-        <Button value="." className="btn" />
-        <Button value="=" className="btn orange" />
+handleClick = (e) => {
+  this.setState((prevState) => calculate(prevState, e.target.textContent));
+};
 
-      </div>
-    );
-  }
+render() {
+  const { total, next, operation } = this.state;
+  return (
+    <div className="button-container">
+      <Button value={next || operation || total || 0} className="btn row1" />
+      <Button value="AC" className="btn" handleClick={this.handleClick} />
+      <Button value="+/-" className="btn" handleClick={this.handleClick} />
+      <Button value="%" className="btn" handleClick={this.handleClick} />
+      <Button value="÷" className="btn orange" handleClick={this.handleClick} />
+      <Button value={7} className="btn" handleClick={this.handleClick} />
+      <Button value={8} className="btn" handleClick={this.handleClick} />
+      <Button value={9} className="btn" handleClick={this.handleClick} />
+      <Button value="x" className="btn orange" handleClick={this.handleClick} />
+      <Button value={4} className="btn" handleClick={this.handleClick} />
+      <Button value={5} className="btn" handleClick={this.handleClick} />
+      <Button value={6} className="btn" handleClick={this.handleClick} />
+      <Button value="-" className="btn orange" handleClick={this.handleClick} />
+      <Button value={1} className="btn" handleClick={this.handleClick} />
+      <Button value={2} className="btn" handleClick={this.handleClick} />
+      <Button value={3} className="btn" handleClick={this.handleClick} />
+      <Button value="+" className="btn orange" handleClick={this.handleClick} />
+      <Button value={0} className="btn col2" handleClick={this.handleClick} />
+      <Button value="." className="btn" handleClick={this.handleClick} />
+      <Button value="=" className="btn orange" handleClick={this.handleClick} />
+    </div>
+  );
+}
 }
